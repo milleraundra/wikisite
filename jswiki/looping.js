74338d9011d33f48245c65bada5f16874bc803de
milleraundra <aundra@millersrock.com>
2016-01-18 19:12:34 -0800

    Update looping.js

--- jswiki/looping.js
+++ jswiki/looping.js
@@ -1,7 +1,8 @@
 $(function(){
-
+$('#loop').append("Ready function ran");
   $('#button').click(function(){
     var array = ['This', ' is', ' an', ' amazing', ' loop! '];
+    var next = 0;
     for (var index = 0; index < array.length; index += 1) {
 
     $('#loop').append(array[index]);
