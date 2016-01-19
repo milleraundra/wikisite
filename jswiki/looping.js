13331f8a262e3485d3e0ffa7f6b12ab7f15002fe
Gaylen Miller <gaylenmiller@gmail.com>
2016-01-18 19:37:40 -0800

    Use incrementing variable instead of a loop

--- jswiki/looping.js
+++ jswiki/looping.js
@@ -1,12 +1,25 @@
 $(function(){
-$('#loop').append("Ready function ran");
-  $('#button').click(function(){
-    var array = ['This', ' is', ' an', ' amazing', ' loop! '];
-    var next = 0;
-    for (var index = 0; index < array.length; index += 1) {
 
-    $('#loop').append(array[index]);
-    }
+	// Section of code to run once when document is ready.	
+	$('#loop').append("Ready function ran");
+   	var words = ['This', ' is', ' an', ' amazing', ' loop! '];
+   	var next = 0;
+
+  	$('#button').click( function() {
+    	// for (var index = 0; index < array.length; index += 1) {
+
+    	// $('#loop').append("testing " + next + ' ' ); // array[index]);
+  		$('#loop').append( words[next] + ' '); // array[index]);
+  		/* next++;
+  		if (next == 5) {
+  			next = 0;
+  		}
+  		*/
+  		// next = (next + 1) % 5;
+  		next = (next === 4) ? 0 : next + 1; 
+  		console.log(next);
+
+   // }
   });
 });
 
